ff5b21d8b2884c9e516087cd73232d3b7effb50f
dmitryagapov <dimago911@gmail.com>
2015-04-12 17:35:07 +0300

    settings

--- filestore/settings/production.py
+++ filestore/settings/production.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # TEMPLATE_DIRS = (
 #     BASE_DIR + '/filestore/file/templates/',
@@ -36,18 +36,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     ('static', BASE_DIR + '/filestore/static/'),
 )
-
-
 # here() gives us file paths from the root of the system to the directory
-# holding the current file.
-here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+# # holding the current file.
+# here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+#
+# PROJECT_ROOT = here("..")
+# # root() gives us file paths from the root of the system to whatever
+# # folder(s) we pass it starting at the parent directory of the current file.
+# root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+#
+#
+# TEMPLATE_DIRS = (
+#     root('templates'),
+# )
 
-PROJECT_ROOT = here("..")
-# root() gives us file paths from the root of the system to whatever
-# folder(s) we pass it starting at the parent directory of the current file.
-root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
-
-
-TEMPLATE_DIRS = (
-    root('templates'),
-)
