11bfb6b223ac572de3cb5b7bf3b0da9528fb2651
dmitryagapov <dimago911@gmail.com>
2015-04-13 00:02:26 +0300

    change db name

--- filestore/settings/production.py
+++ filestore/settings/production.py
@@ -13,7 +13,7 @@ BASE_DIR = settings.BASE_DIR
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_my.sqlite3'),
     }
 }
 
