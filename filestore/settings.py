189d0d84fa85e94826497aeaf47c1a3734739236
dmitryagapov <dimago911@gmail.com>
2015-04-13 01:23:54 +0300

    set

--- filestore/settings.py
+++ filestore/settings.py
@@ -98,4 +98,44 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     ('static', 'C:/Users/Dmitry/DjangoProj/test_0/filestore/static/'),
 )
+###############################33
+DEBUG = False
+TEMPLATE_DEBUG = True
 
+PROJECT_DIRECTORY = os.getcwd()
+
+# Parse database configuration from $DATABASE_URL
+
+
+DATABASES = {
+    'default': dj_database_url.config(),
+}
+
+
+
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+
+
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+    # here you can add another templates directory if you wish.
+)
+
+STATIC_ROOT = os.path.join(PROJECT_DIRECTORY, 'static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    ('static', os.path.join(os.getcwd(), 'static/')),
+)
