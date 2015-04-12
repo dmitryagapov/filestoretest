ef7a7ebdde285dade13e920fe8fc2f5d9be27f7b
dmitryagapov <dimago911@gmail.com>
2015-04-12 17:55:21 +0300

    prod settings

--- filestore/settings/production.py
+++ filestore/settings/production.py
@@ -1,17 +1,13 @@
-from django.conf import settings
+from django.conf import settins
 import os
 
+DATABASES = settins.DATABASES
 DEBUG = False
-
 TEMPLATE_DEBUG = True
-
-DATABASES = settings.DATABASES
-
-
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -21,32 +17,10 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# TEMPLATE_DIRS = (
-#     BASE_DIR + '/filestore/file/templates/',
-#     BASE_DIR + '/filestore/templates/',
-#     BASE_DIR + '/filestore/file/loginsys/templates/',
-#
-# )
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    ('static', BASE_DIR + '/filestore/static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
-# here() gives us file paths from the root of the system to the directory
-# # holding the current file.
-# here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
-#
-# PROJECT_ROOT = here("..")
-# # root() gives us file paths from the root of the system to whatever
-# # folder(s) we pass it starting at the parent directory of the current file.
-# root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
-#
-#
-# TEMPLATE_DIRS = (
-#     root('templates'),
-# )
-
