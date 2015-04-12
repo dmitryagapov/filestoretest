bba3713e26370d3f0b496847a0e6b3f2ca1ec4f6
dmitryagapov <dimago911@gmail.com>
2015-04-12 03:31:04 +0300

    ssss

--- filestore/settings/production.py
+++ filestore/settings/production.py
@@ -11,7 +11,7 @@ DATABASES = settings.DATABASES
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -23,16 +23,31 @@ ALLOWED_HOSTS = ['*']
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-TEMPLATE_DIRS = (
-    BASE_DIR + '/filestore/file/templates/',
-    BASE_DIR + '/filestore/templates/',
-    BASE_DIR + '/filestore/file/loginsys/templates/',
-
-)
+# TEMPLATE_DIRS = (
+#     BASE_DIR + '/filestore/file/templates/',
+#     BASE_DIR + '/filestore/templates/',
+#     BASE_DIR + '/filestore/file/loginsys/templates/',
+#
+# )
 STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     ('static', BASE_DIR + '/filestore/static/'),
+)
+
+
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+
+TEMPLATE_DIRS = (
+    root('templates'),
 )
